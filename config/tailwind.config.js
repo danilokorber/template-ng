e19d3cdcf1ec8290468b275b1c057d89a3562b00
Danilo Koerber (FG-810) <danilo.koerber@bmwgroup.com>
2020-12-11 22:40:58 +0100

    Updated front page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,14 +12,9 @@ module.exports = {
         'top-xl': '0 20px -25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
         'top-2xl': '0 25px -50px -12px rgba(0, 0, 0, 0.25)',
       },
-      fontWeight: {
-        bold: 'bold',
-        normal: 'normal',
-        lighter: 'lighter',
-      },
     },
     fontFamily: {
-      sans: ['BMW'],
+      sans: ['Titillium Web'],
     },
     future: {
       removeDeprecatedGapUtilities: true,
