bc5e981f3fd9a47997189b1823f74e0927dba1d4
Danilo Koerber (FG-810) <danilo@easyware.com.br>
2021-03-26 17:27:34 +0100

    templateVersion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-import { version } from '../../package.json';
+import { templateVersion } from '../../package.json';
 
 export const environment = {
   production: true,
-  version: version,
+  version: templateVersion,
 };
